acac8b845854a9735761f08ae7ca957830dddf95
hendraet <hendrik.raetz@niederjahna.de>
2019-11-27 11:57:04 +0100

    Improces text generation performance and adds support for generating numbers

--- text-generator/text_generation.py
+++ text-generator/text_generation.py
@@ -1,10 +1,16 @@
 import argparse
 import json
+
+import nltk
 import os
 import random
 
 from PIL import Image, ImageDraw, ImageFont
 from nltk.corpus import words
+
+
+def generate_number():
+    return str(random.randint(1000, 1000000))
 
 
 def generate_date():
@@ -14,36 +20,33 @@ def generate_date():
     return '.'.join([day, month, year])
 
 
-def generate_text():
-    # installation of word list is needed on first installation of nltk. Just execute nltk.download() and select the
-    # "words" corpus
-    word_list = words.words()
+def generate_text(word_list):
     return random.choice(word_list)
 
 
 def generate_image(txt):
-    fontsize = 1
+    fontsize = 30
     font_dir = 'google-fonts'
     font_name = random.choice(os.listdir(font_dir))
     font = font_dir + '/' + font_name
-    img_fraction = 0.90
+    target_dimensions = (160, 30)
 
-    # TODO: Irgendwie immer auch gleiche Höhe bringen und Breite egal
-    img = Image.new('RGB', .conf(100, 42), (255, 255, 255))
+    img_font = ImageFont.truetype(font, fontsize)
+    text_dimensions = img_font.getsize(txt)
+
+    img = Image.new('RGB', text_dimensions, (255, 255, 255))
     d = ImageDraw.Draw(img)
 
-    img_font = ImageFont.truetype(font, fontsize)
-    while img_font.getsize(txt)[0] < img_fraction * img.size[0] and \
-            img_font.getsize(txt)[1] < img_fraction * img.size[1]:
-        fontsize += 1
-        img_font = ImageFont.truetype(font, fontsize)
+    d.text((0, 0), txt, fill=(0, 0, 0), font=img_font)
 
-    fontsize -= 1
-    img_font = ImageFont.truetype(font, fontsize)
+    ratio = target_dimensions[1] / text_dimensions[1]
+    new_dimensions = tuple([int(dim * ratio) for dim in text_dimensions])
+    resized_img = img.resize(new_dimensions)
+    padded_img = Image.new('RGB', target_dimensions, (0, 0, 0))
+    padded_img.paste(resized_img, ((target_dimensions[0] - new_dimensions[0]) // 2,
+                                   (target_dimensions[1] - new_dimensions[1]) // 2))
 
-    d.text((5, 5), txt, fill=(0, 0, 0), font=img_font)
-
-    return img, font_name
+    return padded_img, font_name, text_dimensions
 
 
 def main():
@@ -51,8 +54,8 @@ def main():
     parser.add_argument('-d', '--dir', type=str, default='text-generator/generated-images/',
                         help='path to directory where images should be stored')
     parser.add_argument('-n', '--num', type=int, default=1, help='num of images to be generated')
-    parser.add_argument('-t', '--type', type=str, choices=['date', 'text'], default='date', help='type of text to be '
-                                                                                                 'generated')
+    parser.add_argument('-t', '--type', type=str, choices=['date', 'text', 'num'], default='date',
+                        help='type of text to be generated')
     parser.add_argument('-s', '--show', action='store_true', help='show image(s) after generation')
     parser.add_argument('--save', action='store_true', help='save image(s) after generation')
     args = parser.parse_args()
@@ -64,16 +67,26 @@ def main():
     json_path = img_dir + 'data' + '.json'
     img_list = []
 
+    if string_type == 'text':
+        # installation of word list is needed on first installation of nltk.
+        # nltk.download('words')
+        max_length = 9
+        word_list = words.words()
+        word_list = [word for word in word_list if len(word) < max_length]
+
     for i in range(0, args.num):
         if string_type == 'date':
             generated_string = generate_date()
         elif string_type == 'text':
-            generated_string = generate_text()
+            generated_string = generate_text(word_list)
+        elif string_type == 'num':
+            generated_string = generate_number()
         else:
             print('Unexpected type')
             exit(1)
 
-        img, font_name = generate_image(generated_string)
+        img, font_name, text_dimensions = generate_image(generated_string)
+        # print(text_dimensions)
         img_name = generated_string + '-' + os.path.splitext(font_name)[0]
         img_path = img_dir + img_name + '.png'
 
@@ -81,7 +94,8 @@ def main():
             img.show()
 
         if save_image:
-            img.save(img_path, format='PNG')
+            with open(img_path, 'wb+') as img_file:
+                img.save(img_file, format='PNG')
 
         info = {
             'string': generated_string,
