710e60a3e9e913fce56a12b9bb54c57c71b2cc42
hendraet <hendrik.raetz@niederjahna.de>
2020-08-03 17:36:42 +0200

    Adapts prep scripts for easier generation

--- prep/resize_images.py
+++ prep/resize_images.py
@@ -39,47 +39,52 @@ def resize_img(img, target_dimensions, padding_color=0, additional_padding=(0, 0
 
 def main():
     tar_dir = "datasets/tars"
-    outdir = "../handwriting_embedding/datasets"
-    indir = "datasets/"
-    dataset_description = "dataset_descriptions/mixed_gw_rep_words_dates_40k.json"
-    new_name = "mixed_gw_rep_words_dates_40k_padded"
-    # dataset_description = "datasets/tars/iamondb_generated_dates_50k.json"
+    out_dir = "../handwriting_embedding/datasets"
+    in_dir = "datasets/"
+    dataset_description_filename = "dataset_descriptions/mixed_gw_rep_words_dates_40k.json"
+    new_dataset_name = "mixed_gw_rep_words_dates_40k_padded"
+    # dataset_description_filename = "datasets/tars/iamondb_generated_dates_50k.json"
     target_dimensions = (216, 64)
     padding_colour = 255
     additional_padding = True
 
-    image_files = [fn for fn in os.listdir(outdir) if fn.endswith(".png")]
-    # TODO
-    # assert not image_files, "There are already image files in the outdir"
+    image_files = [fn for fn in os.listdir(out_dir) if fn.endswith(".png")]
+    assert not image_files, "There are already image files in the out dir"
 
-    with open(dataset_description, "r") as j_file:
+    with open(dataset_description_filename, "r") as j_file:
         num_json = json.load(j_file)
 
-    # TODO
-    # images = [i["path"] for i in num_json]
-    images = [i["path"] for i in num_json if i["type"] == "date"]
+    images = [i["path"] for i in num_json]
     for img_path in images:
         img_path = os.path.basename(img_path)
-        img = Image.open(join(indir, img_path))
+        img = Image.open(join(in_dir, img_path))
 
         padding_right = max(0, int(random.gauss(0, 35))) if additional_padding else 0
         padded_img = resize_img(img, target_dimensions, padding_colour, additional_padding=(0, 0, padding_right, 0))
 
-        new_img_path = join(outdir, img_path)
+        new_img_path = join(out_dir, img_path)
         with open(new_img_path, "wb") as img_file:
             padded_img.save(img_file, format="PNG")
 
     # copy description
-    new_dataset_description = os.path.join(outdir, new_name + ".json")
-    shutil.copy(dataset_description, new_dataset_description)
+    new_dataset_description = os.path.join(out_dir, new_dataset_name + ".json")
+    shutil.copy(dataset_description_filename, new_dataset_description)
 
-    # make tar
-    tar_filename = os.path.join(tar_dir, new_name + ".tar.bz2")
+    create_tar(new_dataset_name, new_dataset_description, out_dir, tar_dir)
+
+
+def create_tar(new_dataset_name, new_dataset_description, outdir, tar_dir, final_dir=None):
+    tar_filename = os.path.join(tar_dir, new_dataset_name + ".tar.bz2")
     image_files = [os.path.join(outdir, fn) for fn in os.listdir(outdir) if fn.endswith(".png")]
     with tarfile.open(tar_filename, "w:bz2") as tar:
         for filename in image_files:
             tar.add(filename, arcname=os.path.basename(filename))
         tar.add(new_dataset_description, arcname=os.path.basename(new_dataset_description))
+
+    if final_dir is not None:
+        for filename in image_files:
+            shutil.move(filename, final_dir)
+        shutil.move(new_dataset_description, final_dir)
 
 
 if __name__ == "__main__":
--- prep/text_generation.py
+++ prep/text_generation.py
@@ -1,21 +1,18 @@
 import argparse
-from datetime import datetime
 import json
-import time
+import string
+from datetime import datetime
 
-import nltk
 import os
 import random
-import subprocess
-
 from PIL import Image, ImageDraw, ImageFont
 from nltk.corpus import words
 
-from prep.resize_images import resize_img
+from prep.resize_images import resize_img, create_tar
 
 
 def generate_number():
-    if bool(random.getrandbits(1)):
+    if random.choice((True, False)):
         return str(random.randint(1000, 1000000))
     else:
         return str(random.randint(0, 100))
@@ -32,11 +29,11 @@ def generate_date():
     # Month YY
     # DD. Month
     # DD. Mon
-    date_formats = ['%d.%m.%y', '%d.%m.%Y', '%m/%d/%y', '%m/%d/%Y', '%d. %B %Y', '%d. %b %Y', '%B %y', '%d. %B',
-                    '%d. %b', '%d-%m-%y']
+    date_formats = ["%d.%m.%y", "%d.%m.%Y", "%m/%d/%y", "%m/%d/%Y", "%d. %B %Y", "%d. %b %Y", "%B %y", "%d. %B",
+                    "%d. %b", "%d-%m-%y"]
 
-    start = datetime.strptime('01.01.1000', '%d.%m.%Y')
-    end = datetime.strptime('01.01.2020', '%d.%m.%Y')
+    start = datetime.strptime("01.01.1000", "%d.%m.%Y")
+    end = datetime.strptime("01.01.2020", "%d.%m.%Y")
     delta = end - start
 
     rand_date = start + delta * random.random()
@@ -48,18 +45,30 @@ def generate_text(word_list):
     return random.choice(word_list)
 
 
+def generate_alpha_num():
+    length = random.randint(3, 12)
+    possible_chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + "_"
+    x = ''.join(random.choice(possible_chars) for _ in range(length))
+    print(x)
+    return x
+
+
+def generate_plz():
+    return str(random.randint(0, 100000)).zfill(5)
+
+
 def generate_image(txt, add_additional_padding=False):
     fontsize = 70
-    font_dir = '../google-fonts'
+    font_dir = "../google-fonts"
     font_name = random.choice(os.listdir(font_dir))
     # font_name = "Dhurjati-Regular.ttf"
-    font = font_dir + '/' + font_name
+    font = font_dir + "/" + font_name
     target_dimensions = (216, 64)
 
     img_font = ImageFont.truetype(font, fontsize)
     text_dimensions = img_font.getsize(txt)
 
-    img = Image.new('L', text_dimensions, 255)
+    img = Image.new("L", text_dimensions, 255)
     d = ImageDraw.Draw(img)
 
     d.text((0, 0), txt, fill=0, font=img_font)
@@ -74,32 +83,37 @@ def generate_image(txt, add_additional_padding=False):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generating images with random text')
-    parser.add_argument('-d', '--dir', type=str, default='datasets/',
-                        help='path to directory where images should be stored')
-    parser.add_argument('-n', '--num', type=int, default=1, help='num of images to be generated')
-    parser.add_argument('-t', '--type', type=str, choices=['date', 'text', 'num'], default='date',
-                        help='type of text to be generated or replicate strings from json')
-    parser.add_argument('-s', '--show', action='store_true', help='show image(s) after generation')
-    parser.add_argument('-p', '--json_img_path', type=str, default='',
-                        help='dir path that should be written to json')
-    parser.add_argument('--save', action='store_true', help='save image(s) after generation')
-    parser.add_argument('-jo', '--out-json-path', type=str, help='name of the output json file')
-    parser.add_argument('-ji', '--in-json-path', type=str,
-                        help='name of the json file that contains strings that should be replicated')
+    parser = argparse.ArgumentParser(description="Generating images with random text")
+    parser.add_argument("-d", "--intermediate-dir", type=str, default="datasets/",
+                        help="path to directory where image will be stored temporarily. This dir has to be free of "
+                             "images")
+    parser.add_argument("-td", "--tar-dir", type=str, default="datasets/tars",
+                        help="path to directory where the dataset tar should be stored")
+    parser.add_argument("-fd", "--final-dir", type=str,
+                        help="path to directory where the model will read the data from")
+    parser.add_argument("-n", "--num", type=int, default=1, help="num of images to be generated")
+    parser.add_argument("-t", "--type", type=str, choices=["date", "text", "num", "plz", "alpha_num"], default="date",
+                        help="type of text to be generated or replicate strings from json")
+    parser.add_argument("-s", "--show", action="store_true", help="show image(s) after generation")
+    parser.add_argument("-p", "--json_img_path", type=str, default="",
+                        help="dir path that should be prepended to every path in json file")
+    parser.add_argument("--save", action="store_true", help="save image(s) after generation")
+    parser.add_argument("-dn", "--dataset-name", type=str, help="name of the new dataset")
+    parser.add_argument("-ji", "--in-json-path", type=str,
+                        help="name of the json file that contains strings that should be replicated")
     args = parser.parse_args()
 
     string_type = args.type
     show_image = args.show
     save_image = args.save
-    img_dir = args.dir
+    intermediate_dir = args.intermediate_dir
     json_img_dir = args.json_img_path
-    json_path = img_dir + args.out_json_path + '.json'
+    out_json_path = intermediate_dir + args.dataset_name + ".json"
     img_list = []
 
-    if string_type == 'text':
+    if string_type == "text":
         # installation of word list is needed on first installation of nltk.
-        # nltk.download('words')
+        # nltk.download("words")
         max_length = 9
         word_list = words.words()
         word_list = [word for word in word_list if len(word) < max_length]
@@ -112,47 +126,53 @@ def main():
             dataset.append((entry["string"], entry["type"]))
     else:
         for i in range(0, args.num):
-            if string_type == 'date':
+            if string_type == "date":
                 dataset.append((generate_date(), string_type))
-            elif string_type == 'text':
+            elif string_type == "text":
                 dataset.append((generate_text(word_list), string_type))
-            elif string_type == 'num':
+            elif string_type == "num":
                 dataset.append((generate_number(), string_type))
+            elif string_type == "plz":
+                dataset.append((generate_plz(), string_type))
+            elif string_type == "alpha_num":
+                dataset.append((generate_alpha_num(), string_type))
             else:
-                print('Unexpected type')
+                print("Unexpected type")
                 exit(1)
+
+    if save_image:
+        image_files = [fn for fn in os.listdir(intermediate_dir) if fn.endswith(".png")]
+        assert not image_files, "There are already image files in the out dir"
 
     for generated_string, string_type in dataset:
         img, font_name, text_dimensions = generate_image(generated_string)
         cleansed_string = generated_string.replace("/", "_").replace(" ", "_")
-        img_name = cleansed_string + '-' + os.path.splitext(font_name)[0]
-        img_path = img_dir + img_name + '.png'
-        json_img_path = json_img_dir + img_name + '.png'
+        img_name = cleansed_string + "-" + os.path.splitext(font_name)[0]
+        img_path = intermediate_dir + img_name + ".png"
+        json_img_path = json_img_dir + img_name + ".png"
 
         if show_image:
             img.show()
 
         if save_image:
-            with open(img_path, 'wb+') as img_file:
-                img.save(img_file, format='PNG')
+            with open(img_path, "wb+") as img_file:
+                img.save(img_file, format="PNG")
 
         info = {
-            'string': generated_string,
-            'type': string_type,
-            'path': json_img_path,
-            'font': font_name
+            "string": generated_string,
+            "type": string_type,
+            "path": json_img_path,
+            "font": font_name
         }
         img_list.append(info)
 
-    # print(img_list)
     if save_image:
-        with open(json_path, 'w+') as json_file:
+        with open(out_json_path, "w+") as json_file:
             json.dump(img_list, json_file, indent=4)
 
-    # TODO: tar and move to desired dir/clean up
-    # move JSON to datasetdescr
+    create_tar(args.dataset_name, out_json_path, intermediate_dir, args.tar_dir, args.final_dir)
 
 
-if __name__ == '__main__':
-    # assert False, 'Dates DD Month YYYY have to be fixed'
+if __name__ == "__main__":
+    # assert False, "Dates DD Month YYYY have to be fixed"
     main()
