730cfe8c99937eb780bd4078ce968ef66090dc8d
hendraet <hendrik.raetz@niederjahna.de>
2020-09-04 16:06:46 +0200

    Adds train sample limit option

--- prep/create_train_test_set.py
+++ prep/create_train_test_set.py
@@ -8,32 +8,45 @@ json_paths = ["ganwriting_generated_words_nums_dates_18k.json", "alpha_num_gener
               "gw_baseline_replicated.json", "nums_generated_20k.json", "plzs_generated_20k.json"]
 dataset_dir = "../handwriting_embedding/datasets"
 tar_dir = "datasets/tars"
-new_dataset_name = "full_ds_nums_text_only"
+new_dataset_name = "nums_plzs"
 
-allowed_classes = ["num", "text"]
-print(f"Using only camples of classes: {', '.join(allowed_classes)}")
+allowed_classes = ["num", "plz"]
+# allowed_classes = None
+if allowed_classes is not None:
+    print(f"Using only samples of classes: {', '.join(allowed_classes)}")
+
+# train_set_sample_limit = 100
+train_set_sample_limit = None
+if train_set_sample_limit is not None:
+    print(f"Limiting train samples to {train_set_sample_limit} for each class.")
 
 dataset = {}
 for dataset_description_path in json_paths:
-    with open(os.path.join(dataset_dir, dataset_description_path), 'r') as f:
+    with open(os.path.join(dataset_dir, dataset_description_path), "r") as f:
         json_file = json.load(f)
 
     for sample in json_file:
-        if sample["type"] not in allowed_classes:
+        if allowed_classes is not None and sample["type"] not in allowed_classes:
             continue
-        if sample['type'] not in dataset:
-            dataset[sample['type']] = []
-        dataset[sample['type']].append(sample)
+
+        if sample["type"] not in dataset:
+            dataset[sample["type"]] = []
+        dataset[sample["type"]].append(sample)
 classes = dataset.keys()
 
 train = []
 test = []
 smallest_class_len = min([len(ds) for ds in dataset.values()])
-threshold = int(smallest_class_len * 0.9)
+if train_set_sample_limit is None:
+    threshold = int(smallest_class_len * 0.9)
+else:
+    assert train_set_sample_limit < smallest_class_len
+    threshold = train_set_sample_limit
 for samples in dataset.values():
     random.shuffle(samples)  # Makes sure that no internal structure in the json file messes up dataset
     train.extend([sample for sample in samples[:threshold]])
     test.extend([sample for sample in samples[threshold:smallest_class_len]])
+
 
 random.shuffle(train)
 random.shuffle(test)
