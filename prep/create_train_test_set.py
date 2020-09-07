92d8c4a406fba1c987a0c75a3f7f22acb11dfa36
hendraet <hendrik.raetz@niederjahna.de>
2020-09-07 09:46:21 +0200

    Adds parameter that steers number of samples in test set

--- prep/create_train_test_set.py
+++ prep/create_train_test_set.py
@@ -8,17 +8,22 @@ json_paths = ["ganwriting_generated_words_nums_dates_18k.json", "alpha_num_gener
               "gw_baseline_replicated.json", "nums_generated_20k.json", "plzs_generated_20k.json"]
 dataset_dir = "../handwriting_embedding/datasets"
 tar_dir = "datasets/tars"
-new_dataset_name = "nums_plzs"
+new_dataset_name = "full_ds_30_train_samples"
 
-allowed_classes = ["num", "plz"]
-# allowed_classes = None
+# allowed_classes = ["num", "plz"]
+allowed_classes = None
 if allowed_classes is not None:
     print(f"Using only samples of classes: {', '.join(allowed_classes)}")
 
-# train_set_sample_limit = 100
-train_set_sample_limit = None
+train_set_sample_limit = 30
+# train_set_sample_limit = None
 if train_set_sample_limit is not None:
     print(f"Limiting train samples to {train_set_sample_limit} for each class.")
+
+test_set_sample_limit = 30
+# test_set_sample_limit = None
+if test_set_sample_limit is not None:
+    print(f"Limiting train samples to {test_set_sample_limit} for each class.")
 
 dataset = {}
 for dataset_description_path in json_paths:
@@ -36,16 +41,20 @@ classes = dataset.keys()
 
 train = []
 test = []
-smallest_class_len = min([len(ds) for ds in dataset.values()])
+max_len_per_class = min([len(ds) for ds in dataset.values()])
 if train_set_sample_limit is None:
-    threshold = int(smallest_class_len * 0.9)
+    threshold = int(max_len_per_class * 0.9)
 else:
-    assert train_set_sample_limit < smallest_class_len
+    assert train_set_sample_limit < max_len_per_class
     threshold = train_set_sample_limit
+
+if test_set_sample_limit is not None:
+    max_len_per_class = threshold + test_set_sample_limit
+
 for samples in dataset.values():
     random.shuffle(samples)  # Makes sure that no internal structure in the json file messes up dataset
     train.extend([sample for sample in samples[:threshold]])
-    test.extend([sample for sample in samples[threshold:smallest_class_len]])
+    test.extend([sample for sample in samples[threshold:max_len_per_class]])
 
 
 random.shuffle(train)
