7b5d956f3397b51ead835fb19ee8f2a90d897dd8
hendraet <hendrik.raetz@niederjahna.de>
2021-01-05 17:05:40 +0100

    Adds support for validation set creation to train_test_spit script

--- prep/dataset_processing/create_train_test_set.py
+++ prep/dataset_processing/create_train_test_set.py
@@ -8,20 +8,19 @@ json_paths = ["ganwriting_generated_words_nums_dates_18k.json", "alpha_num_gener
               "gw_baseline_replicated.json", "nums_generated_20k.json", "plzs_generated_20k.json"]
 dataset_dir = "../handwriting_embedding/datasets"
 tar_dir = "datasets/tars"
-new_dataset_name = "full_ds_30_train_samples"
+new_dataset_name = "5CHPT"
+create_validation_set = True
 
 # allowed_classes = ["num", "plz"]
 allowed_classes = None
 if allowed_classes is not None:
     print(f"Using only samples of classes: {', '.join(allowed_classes)}")
 
-train_set_sample_limit = 30
-# train_set_sample_limit = None
+train_set_sample_limit = None
 if train_set_sample_limit is not None:
     print(f"Limiting train samples to {train_set_sample_limit} for each class.")
 
-test_set_sample_limit = 30
-# test_set_sample_limit = None
+test_set_sample_limit = None
 if test_set_sample_limit is not None:
     print(f"Limiting train samples to {test_set_sample_limit} for each class.")
 
@@ -41,9 +40,13 @@ classes = dataset.keys()
 
 train = []
 test = []
+val = []
 max_len_per_class = min([len(ds) for ds in dataset.values()])
 if train_set_sample_limit is None:
-    threshold = int(max_len_per_class * 0.9)
+    if create_validation_set:
+        threshold = int(max_len_per_class * 0.8)
+    else:
+        threshold = int(max_len_per_class * 0.9)
 else:
     assert train_set_sample_limit < max_len_per_class
     threshold = train_set_sample_limit
@@ -51,16 +54,22 @@ else:
 if test_set_sample_limit is not None:
     max_len_per_class = threshold + test_set_sample_limit
 
+second_threshold = threshold + (max_len_per_class - threshold) // 2
 for samples in dataset.values():
     random.shuffle(samples)  # Makes sure that no internal structure in the json file messes up dataset
     train.extend([sample for sample in samples[:threshold]])
-    test.extend([sample for sample in samples[threshold:max_len_per_class]])
+    if create_validation_set:
+        val.extend([sample for sample in samples[threshold:second_threshold]])
+        test.extend([sample for sample in samples[second_threshold:max_len_per_class]])
+    else:
+        test.extend([sample for sample in samples[threshold:max_len_per_class]])
 
 
 random.shuffle(train)
 random.shuffle(test)
+random.shuffle(val)
 
-print(f"Saving {len(train)}/{len(test)} samples")
+print(f"Saving {len(train)}/{len(val)}/{len(test)} samples")
 
 # make json
 train_set_path = os.path.join(dataset_dir, f"{new_dataset_name}_train.json")
@@ -71,8 +80,15 @@ test_set_path = os.path.join(dataset_dir, f"{new_dataset_name}_test.json")
 with open(test_set_path, "w") as test_out_file:
     json.dump(test, test_out_file, indent=4)
 
-tar_filename = os.path.join(tar_dir, new_dataset_name + "_train_test.tar.bz2")
-image_files = [sample["path"] for sample in test + train]
+if create_validation_set:
+    val_set_path = os.path.join(dataset_dir, f"{new_dataset_name}_val.json")
+    with open(val_set_path, "w") as val_out_file:
+        json.dump(val, val_out_file, indent=4)
+    tar_filename = os.path.join(tar_dir, new_dataset_name + "_train_val_test.tar.bz2")
+else:
+    tar_filename = os.path.join(tar_dir, new_dataset_name + "_train_test.tar.bz2")
+
+image_files = [sample["path"] for sample in test + val + train]
 with tarfile.open(tar_filename, "w:bz2") as tar:
     for i, filename in enumerate(image_files):
         if (i + 1) % (len(image_files) // 10) == 0:
@@ -80,3 +96,5 @@ with tarfile.open(tar_filename, "w:bz2") as tar:
         tar.add(os.path.join(dataset_dir, filename), arcname=filename)
     tar.add(train_set_path, arcname=os.path.basename(train_set_path))
     tar.add(test_set_path, arcname=os.path.basename(test_set_path))
+    if create_validation_set:
+        tar.add(val_set_path, arcname=os.path.basename(val_set_path))
